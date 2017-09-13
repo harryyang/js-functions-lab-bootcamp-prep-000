c5619ae041fe050ffd34b2a5b1ae3dc112312fcc
Harold Yang <harold.h.yang@gmail.com>
2017-09-13 18:54:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,15 +2,3 @@
 function happyHolidays() {
   return 'Happy Holidays!'
 }
-
-function happyHolidaysTo(name) {
-  return 'Happy Holidays, ${name}!'
-}
-
-function happyHolidayTo (holiday, name){
-  return 'Happy ${holiday}, ${name}!'
-}
-
-function holidayCountdown (days, holiday) {
-  return 'It's ${days} days until ${holiday}!
-}
